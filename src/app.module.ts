09d38292f0feff7ef2cba9f64dc96249bf6d594c
vasili <frugas@list.ru>
2025-09-19 15:18:31 +0300

    add fximes & todos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
-//TODO: удалить закомментированный код после проверки работоспособности
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-// import { ClientsModule, Transport } from '@nestjs/microservices';
 import { envSchema } from './config/validation';
 import { RmqModule } from './common/rmq/rmq.module';
 import { UsersModule } from './modules/users/users.module';
@@ -11,7 +9,6 @@ import { HealthModule } from './modules/health/health.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      // cache: true,
       isGlobal: true,
       validationSchema: envSchema,
       envFilePath:
@@ -19,34 +16,6 @@ import { HealthModule } from './modules/health/health.module';
       expandVariables: true,
     }),
     RmqModule.forServices(),
-    // ClientsModule.registerAsync([
-    //   {
-    //     name: 'AUTH_CLIENT',
-    //     imports: [ConfigModule],
-    //     inject: [ConfigService],
-    //     useFactory: (cfg: ConfigService) => ({
-    //       transport: Transport.RMQ,
-    //       options: {
-    //         urls: [cfg.getOrThrow<string>('RMQ_URL')],
-    //         queue: cfg.get<string>('RMQ_AUTH_QUEUE'),
-    //         queueOptions: { durable: true },
-    //       },
-    //     }),
-    //   },
-    //   {
-    //     name: 'USERS_CLIENT',
-    //     imports: [ConfigModule],
-    //     inject: [ConfigService],
-    //     useFactory: (cfg: ConfigService) => ({
-    //       transport: Transport.RMQ,
-    //       options: {
-    //         urls: [cfg.getOrThrow<string>('RMQ_URL')],
-    //         queue: cfg.get<string>('RMQ_USERS_QUEUE'),
-    //         queueOptions: { durable: true },
-    //       },
-    //     }),
-    //   },
-    // ]),
     HealthModule,
     UsersModule,
     AuthModule,
